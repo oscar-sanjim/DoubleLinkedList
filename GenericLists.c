6517feed2462c0b28ce42fa57346b1cb96adfd61
Jacob Rivera <edjacob25@gmail.com>
2015-02-27 20:42:06 -0600

    Added comments

--- GenericLists.c
+++ GenericLists.c
@@ -13,12 +13,15 @@ list_p CreateList (int (*destroy) (const void *data_p),
                                         int key),
                       void *  (* copy)(const void *source_p)) {
 
+	/* Alloc meemory for the list */
 	list_p newList =(list_p) malloc(sizeof(list));
 
+	/* Set the variables to a default value */
 	newList->numItems = 0;
 	newList->head_p = NULL;
 	newList->tail_p = NULL;
 
+	/* Assign the methods to the list */
 	newList->destroy = destroy;
 	newList->print = print;
 	newList->compare = compare;
@@ -28,17 +31,19 @@ list_p CreateList (int (*destroy) (const void *data_p),
 }
 
 int DestroyList(list_p myList_p){
-	if (myList_p == NULL)
+	if (myList_p == NULL) /* Check if the list is valid */
 	{
 		return EXIT_FAILURE;
 	}
 	else{
 		node_p temp;
+		/* Run throught the list, delete the data from each node and then free each node */
 		for (temp =(node_p)myList_p->head_p; temp != NULL; temp = temp->next_p)
 		{
 		 	myList_p->destroy(temp->data_p);
 		 	free(temp);
 		}
+		/* Free the structure of the list */
 		free(myList_p); 
 		return EXIT_SUCCESS;
 	}
@@ -46,17 +51,17 @@ int DestroyList(list_p myList_p){
 
 
 int Delete(list_p myList_p, node_p item_p, void **data_h){
+	/* Check if the list is valid */
 	if (myList_p == NULL){
 		return EXIT_FAILURE;
 	}
 	else {
 		node_p temp;
-		*data_h = item_p->data_p;
-		//printf("lol\n");
-		myList_p->print(*data_h);
+		/* In case the item is a NULL, delete the head */
 		if(item_p == NULL)
 		{
 			temp = myList_p->head_p;
+			*data_h = temp->data_p;
 			myList_p->head_p = temp->next_p;
 		 	temp->next_p->prev_p = NULL;
 		 	free(temp);
@@ -64,29 +69,38 @@ int Delete(list_p myList_p, node_p item_p, void **data_h){
 		}
 		else
 		{
+			/* Assign the handler to the data of the node that will be deleted */
+			*data_h = item_p->data_p;
+
+			/* Run throught the list and find the node that has to be deleted*/
 			for (temp = myList_p->head_p; temp != NULL; temp = temp->next_p)
 			{
+				/* Node found */
 				if (myList_p->compare(temp->data_p,item_p->data_p,2) == 0)
 			 	{
-			 		data_h = &item_p->data_p;
-
+			 		/*data_h = &item_p->data_p;*/
+			 		/* If the node is the head, assign a new head */
 			 		if(myList_p->compare(temp->data_p,myList_p->head_p->data_p,2) == 0)
 			 		{
 			 			myList_p->head_p = temp->next_p;
 			 			temp->next_p->prev_p = NULL;
 			 		}
 			 		else{
+			 			/* If the node is the tail, assign a new tail */
 			 			if(myList_p->compare(temp->data_p,myList_p->tail_p->data_p,2) == 0)
 				 		{
 				 			myList_p->tail_p = temp->prev_p;
 				 			temp->prev_p->next_p = NULL;
 				 		}
+				 		/* If the node is in the middle, connect the last and the next */
 				 		else
 				 		{
 				 			temp->prev_p->next_p = temp->next_p;
 				 			temp->next_p->prev_p = temp->prev_p;
 				 		}
 			 		}
+
+			 		/* Free the node and decrement the number of elments*/
 			 		free(temp);
 			 		myList_p->numItems--;
 
@@ -99,13 +113,18 @@ int Delete(list_p myList_p, node_p item_p, void **data_h){
 }
 
 int Insert(list_p myList_p, node_p item_p, const void *data_p){
+	/* Check that the list is valid */
 	if (myList_p == NULL)
 	{
 		return EXIT_FAILURE;
 	}
 	else {
+		/* Allocate memory for the new node */
 		node_p temp = (node_p) malloc(sizeof(node));
+		/* Assign the data*/
 		temp->data_p = data_p;
+
+		/* If it's the first element of the list */
 		if(myList_p->numItems == 0)
 		{
 			myList_p->head_p = temp;
@@ -115,51 +134,59 @@ int Insert(list_p myList_p, node_p item_p, const void *data_p){
 			myList_p->numItems++;
 		}
 		else {
+			/* Assign at the tail */
 			if (item_p == NULL)
 			{
 				myList_p->tail_p->next_p = temp;
 				temp->next_p = NULL;
 				temp->prev_p =  myList_p->tail_p;
 				myList_p->tail_p = temp;
-				myList_p->numItems++;
 			}
 			else
-			{
+			{ 
 				if(NULL == item_p->prev_p) /*Checking node for head*/
 				{
 					myList_p->head_p = temp;
 				}
-				else
+				else /* Not the head */
 				{
 					item_p->prev_p->next_p = temp;
 				}
+				/* Insert the new node*/
 				temp->prev_p = item_p->prev_p;
 				temp->next_p = item_p;
 				item_p->prev_p = temp;
 
 			}
+			myList_p->numItems++;
 		}
 		return EXIT_SUCCESS;
 	}
 }
 
 list_p DuplicateList (list_p sourceList_p) {
-	list_p newlist =(list_p) malloc(sizeof(list));
-	node_p temp =(node_p)  malloc(sizeof(node));
-	node_p iterator =(node_p) malloc(sizeof(node));
-
+	
+	/* If the list is not empty */
 	if(NULL != sourceList_p->head_p)
 	{
+		/* Create a new list and a comuple of nodes */
+		list_p newlist =(list_p) malloc(sizeof(list));
+		node_p temp =(node_p)  malloc(sizeof(node));
+		node_p iterator;
+
+		/* Copy the data for the first node */
 		temp->data_p = sourceList_p->copy(temp->data_p);
 		temp->prev_p = NULL;
 		newlist->head_p = temp;
 		newlist->numItems = 1;
 	
-
+		/* Run throught the list, for each node, create a new one and copy the data to the new*/
 		for(iterator=sourceList_p->head_p->next_p; iterator!=NULL; iterator=iterator->next_p)
 		{
 			node_p created =(node_p) malloc(sizeof(node));
 			created->data_p = sourceList_p->copy(iterator->data_p);
+			
+			/* Interlink the new nodes */
 			created->prev_p = temp;
 			temp->next_p = created;
 
@@ -167,6 +194,7 @@ list_p DuplicateList (list_p sourceList_p) {
 			newlist->numItems++;
 		}
 
+		/* Assign the tail to the last element */
 		temp->next_p = NULL;
 		newlist->tail_p = temp;
 
@@ -181,6 +209,7 @@ int SortList (list_p myList_p, int key){
 }
 
 int PrintList(list_p myList_p){
+	/* Check that the llist is valid */
 	if (myList_p == NULL)
 	{
 		return EXIT_FAILURE;
@@ -188,6 +217,7 @@ int PrintList(list_p myList_p){
 	else
 	{
 		node_p temp;
+		/* Run througt the list and call the print method in each element */
 		for (temp = myList_p->head_p; temp != NULL; temp = temp->next_p)
 		{
 			myList_p->print(temp->data_p);
@@ -199,6 +229,7 @@ int PrintList(list_p myList_p){
 
 node_p FindInList (list_p myList_p, const void *value_p, int key){
 	node_p temp;
+	/* Run through the list, comparing each value */
 	for (temp = myList_p->head_p; temp!= NULL; temp = temp->next_p)
 	{
 		if (myList_p->compare(temp->data_p, value_p, key) == EQUAL)
--- UserDefined.c
+++ UserDefined.c
@@ -6,32 +6,38 @@
 
 int PrintItem(const void *data)
 {
+	/* Create a cast pointer and asign the parameter */
 	myData *cast;
 	cast = (myData *)data;	
+	/* Print the variables */
 	printf("%d %s\n",cast->number,cast->theString);
 	return EXIT_SUCCESS;
 }
 
 int FreeItem (const void *data){
+	/* Create a cast pointer and asign the parameter */
 	myData * cast;
 	cast = (myData *)data;
+	/* Free the String and then, the structure*/
 	free(cast->theString);
-	//cast->number = 0;
 	free(cast);
+
 	return EXIT_SUCCESS;
 }
 
 
 int CompareItems (const void *item1_p, const void *item2_p, int key){
-	
+	/* Create a cast pointer and asign the parameter */
 	myData *valor1;
 	valor1 = (myData *)item1_p;
 
-	if((key == INT) || (key == STR))
+	if((key == INT) || (key == STR)) /* Comparing two nodes */
 	{
+		/* Create a cast pointer and asign the second parameter */
 		myData *valor2;
 		valor2 = (myData *)item2_p;
 
+		/* Compare the nodes by numeric value */
 		if(key == INT)
 		{
 			if(valor1->number  == valor2->number) return EQUAL;
@@ -39,6 +45,7 @@ int CompareItems (const void *item1_p, const void *item2_p, int key){
 			else if(valor1->number  >  valor2->number) return GREATER;
 		}
 
+		/* Compare the nodes by string */
 		if(key == STR)
 		{
 			if(strcmp(valor1->theString,valor2->theString) == 0) return EQUAL;
@@ -47,21 +54,27 @@ int CompareItems (const void *item1_p, const void *item2_p, int key){
 	}
 
 
-
+	/* Comparing a node and a String */
 	if(key == SINGLESTR)
 	{
 		if(strcmp(valor1->theString,item2_p) == 0) return EQUAL;
 		else return NOTEQUAL;
 	}
-
+	/* Return an error if the key is wrong */
 	return ERROR;
 }
 
 void * CopyItems (const void *source_p){
-	myData * newNode = malloc(sizeof(myData));
+	/* Create a cast pointer and asign the parameter */
 	myData * cast = (myData *)source_p;
+	
+	/* Alloc memory for the new copy of the data */
+	myData * newNode = malloc(sizeof(myData));
+	/* Asign the number of the old data*/
 	newNode->number = cast->number;
+	/* Alloc memory for the string of the old data and then copy it*/
 	newNode->theString = malloc(sizeof(cast->theString));
 	strcpy(newNode->theString,cast->theString);
+	
 	return newNode;
 }
